5578383011dfac6f363f98b79d737940a0ce355a
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-08-25 07:54:17 +0500

    new fields added

--- server/student/models.py
+++ server/student/models.py
@@ -2,26 +2,39 @@ from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext as _
 import uuid
+# My Files
+from questionbank.models import QuestionBank
+from course.models import Course
+from bundle.models import Bundle
 
 
 # Student model
 class Student(models.Model):
     # Unique identifier for each student
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    
     # Student personal information
-    first_name = models.CharField(_("First Name"), max_length=50, null=False)
-    last_name = models.CharField(_("Last Name"), max_length=50, null=False)
-    email = models.EmailField(_("Email"), max_length=254, null=False, unique=True)
-    username = models.CharField(_("Username"), max_length=50, null=False, unique=True)
-    password = models.CharField(_("Password"), max_length=50, null=False)
+    first_name = models.CharField(_("First Name"), max_length=50, null=False, blank=False)
+    last_name = models.CharField(_("Last Name"), max_length=50, null=False, blank=False)
+    email = models.EmailField(_("Email"), max_length=254, null=False, unique=True, blank=False)
+    username = models.CharField(_("Username"), max_length=50, null=False, unique=True, blank=False)
+    password = models.CharField(_("Password"), max_length=50, null=False, blank=False)
+    
     # Authentication status
     is_authenticated = models.BooleanField(_("Is Authenticated"), default=False)
+    
     # Relationship with other models
     # Each student can take multiple question banks, courses, and bundles
     # Each question bank, course, and bundle can be taken by a same student
-    taking_questionBanks = models.ManyToManyField('QuestionBank', blank=True)
-    taking_courses = models.ManyToManyField('Course', blank=True)
-    taking_bundles = models.ManyToManyField('Bundle', blank=True)
+    taking_questionBanks = models.ManyToManyField(QuestionBank, blank=True)
+    taking_courses = models.ManyToManyField(Course, blank=True)
+    taking_bundles = models.ManyToManyField(Bundle, blank=True)
+    
+    # Streaks and scores 
+    daysStreak = models.IntegerField(_("Days Streak"), default=0)
+    questionSolved = models.IntegerField(_("Question Solved"), default=0)
+    questionRemained = models.IntegerField(_("Question Remained"), default=0)
+    
     # Timestamps for student creation and update
     timestamp = models.DateTimeField(_("Timestamp"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated At"), auto_now=True)

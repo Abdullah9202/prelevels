0ded3206bca99b87fce23ca3fe39f36835c616bc
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-08-28 09:10:07 +0500

    New inlines added

--- server/questionbank/admin.py
+++ server/questionbank/admin.py
@@ -4,6 +4,27 @@ from .models import (
     QuestionBank, Question, Option, WhyCorrectOption
 )
 
+
+# In line settings for the admin panel
+class WhyCorrectOptionInline(admin.StackedInline):
+    model = WhyCorrectOption
+    extra = 1
+
+
+class OptionInline(admin.StackedInline):
+    model = Option
+    extra = 4
+    inlines = [WhyCorrectOptionInline]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+
+    list_display = ('question_text', 'question_bank', 'year', 'category', 'subject', 'topic', 'is_active')
+    search_fields = ('question_text', 'question_bank__name', 'category', 'subject')
+    list_filter = ('question_bank', 'year', 'category', 'subject')
+
+
 # Registering the Bundle model
 admin.site.register(
     [QuestionBank, Question, Option, WhyCorrectOption]

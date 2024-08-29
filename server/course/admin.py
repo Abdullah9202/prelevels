3aeb7d7ac30fc0239e505793b27f214e687cdcb4
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-08-29 10:50:46 +0500

    course app updated

--- server/course/admin.py
+++ server/course/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 # My Files
 from .models import Course
 
-# Registering the Bundle model
-admin.site.register(Course)
+
+# Courses with Admin
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'discount', 'validity', 'is_active')
+    search_fields = ('name', 'description')
+    list_filter = ('is_active', 'price', 'validity')
+
+
+# Registering the Course model
+admin.site.register(Course, CourseAdmin)

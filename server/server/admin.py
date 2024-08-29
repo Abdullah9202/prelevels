fd1a99b37bf928784dc784e3913cce6f9456e1c0
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-08-29 14:07:14 +0500

    main server app updated

---
+++ server/server/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Customizing the admin site
+admin.site.site_header = "Prelevels Admin Panel"
+admin.site.site_title = "Prelevels Admin"
+admin.site.index_title = "Welcome to Prelevels Admin"
--- server/server/settings.py
+++ server/server/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Main App
+    'server',
     # My Apps
     'hello', # Testing app
     'student',

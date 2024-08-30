5a0013707381a45827613338022c8969d1fc836b
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-08-30 20:53:57 +0500

    router added in api

--- server/authentication/api.py
+++ server/authentication/api.py
@@ -1,6 +1,9 @@
 from ninja import NinjaAPI
 # My Files
-
+from .router import auth_router
 
 # API Init
 api = NinjaAPI(version="1.0.0", urls_namespace="authentication")
+
+# Including the auth_router
+api.add_router("/auth/", auth_router)

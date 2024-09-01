c5267afd1891c5fb6d8e5f660218e39d1552168a
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-01 21:40:39 +0500

    ninja apps added

--- server/server/settings.py
+++ server/server/settings.py
@@ -41,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party apps
+    'corsheaders',
+    'ninja_extra',
+    'ninja_jwt',
     # Main App
     'server',
     # My Apps

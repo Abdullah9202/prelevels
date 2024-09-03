c9d09569519af20c612367b115506448229a2d97
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-03 21:13:19 +0500

    course schema updated

--- server/course/schemas.py
+++ server/course/schemas.py
@@ -1,16 +1,20 @@
 from uuid import UUID
 from datetime import datetime
-from ninja import Schema
+from ninja import Schema, File
 from typing import Optional
 
 # Schema for Course model
 class CourseSchema(Schema):
     id: UUID
-    name: str
-    description: Optional[str]
-    additional_details: Optional[str]
-    price: Optional[float]
-    validity: Optional[int]
+    name: str = None
+    description: Optional[str] = None
+    additional_details: Optional[str] = None
+    course_image: Optional[File] = None
+    whatsapp_link: str = None
+    resource_link: str = None
+    price: Optional[float] = None
+    discount: Optional[int] = None
+    validity: Optional[int] = None
     created_at: datetime
     updated_at: datetime
-    is_active: bool
+    is_active: bool = True

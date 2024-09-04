a21387431d10a7006c9941094e8287536df8563c
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-04 06:28:56 +0500

    course schema modified

--- server/course/schemas.py
+++ server/course/schemas.py
@@ -5,16 +5,16 @@ from typing import Optional
 
 # Schema for Course model
 class CourseSchema(Schema):
-    id: UUID
-    name: str = None
-    description: Optional[str] = None
+    id: Optional[UUID] = None
+    name: str
+    description: str = None
     additional_details: Optional[str] = None
-    course_image: Optional[File] = None
+    course_image: Optional[str] = None
     whatsapp_link: str = None
     resource_link: str = None
-    price: Optional[float] = None
+    price: float = None
     discount: Optional[int] = None
     validity: Optional[int] = None
-    created_at: datetime
-    updated_at: datetime
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
     is_active: bool = True

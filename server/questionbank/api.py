7ce938229de82556e151eaecb88c47bbadc257d9
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-04 11:22:25 +0500

    NinjaExrtaAPI created and registered

--- server/questionbank/api.py
+++ server/questionbank/api.py
@@ -1,4 +1,7 @@
-from ninja import NinjaAPI
+# Django imports
+# Ninja imports
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.controller import NinjaJWTDefaultController
 # My Files
 from .schemas import (
     QuestionBankSchema, QuestionSchema, DetailedQuestionBankSchema,
@@ -7,4 +10,10 @@ from .schemas import (
 
 
 # API init
-api = NinjaAPI(version="1.0.0", urls_namespace="questionbank")
+api = NinjaExtraAPI(version="1.0.0", urls_namespace="questionbank")
+# Auth Token
+api.register_controllers(NinjaJWTDefaultController)
+
+
+# Including the router from question bank
+api.add_router("", "questionbank.router.question_bank_router")

b85515e6a3a77f83166bb7f690bbf5fa4464d452
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-04 19:15:35 +0500

    bundle schema and details schema added

--- server/bundle/schemas.py
+++ server/bundle/schemas.py
@@ -8,10 +8,24 @@ from typing import Optional
 class BundleSchema(Schema):
     id: UUID
     name: str
-    description: Optional[str]
-    additional_details: Optional[str]
-    price: Optional[float]
-    validity: Optional[int]
-    created_at: datetime
-    updated_at: datetime
-    is_active: bool
+    description: Optional[str] = None
+    additional_details: Optional[str] = None
+    price: int
+    discount: Optional[int] = None
+    validity: Optional[int] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+    is_active: bool = True
+
+
+class BundleDetailSchema(Schema):
+    id: UUID
+    name: str
+    description: Optional[str] = None
+    additional_details: Optional[str] = None
+    price: int 
+    discount: Optional[int] = None  
+    validity: Optional[int] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+    is_active: bool = True

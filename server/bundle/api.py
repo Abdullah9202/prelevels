a7aa880555103f182a75c7ec49377728738c091d
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-04 19:14:39 +0500

    bundle router added in api

--- server/bundle/api.py
+++ server/bundle/api.py
@@ -1,7 +1,15 @@
-from ninja import NinjaAPI
+# Ninja imports
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.controller import NinjaJWTDefaultController
 # My Files
 from .schemas import BundleSchema
 
 
 # API init
-api = NinjaAPI(version="1.0.0", urls_namespace="bundle")
+api = NinjaExtraAPI(version="1.0.0", urls_namespace="bundle")
+# Auth Token
+api.register_controllers(NinjaJWTDefaultController)
+
+
+# Including the router from bundle
+api.add_router("", "bundle.router.bundle_router")

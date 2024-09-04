c79deb6a642a7dda02af23e1ba0196070a222c5b
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-04 09:24:30 +0500

    new schema added

--- server/course/schemas.py
+++ server/course/schemas.py
@@ -1,11 +1,27 @@
 from uuid import UUID
 from datetime import datetime
-from ninja import Schema, File
+from ninja import Schema
 from typing import Optional
 
-# Schema for Course model
-class CourseSchema(Schema):
-    id: Optional[UUID] = None
+# Schemas for Course model
+class GetCourseDetailSchema(Schema):
+    id: UUID
+    name: str
+    description: str = None
+    additional_details: Optional[str] = None
+    course_image: Optional[str] = None
+    whatsapp_link: str = None
+    resource_link: str = None
+    price: float = None
+    discount: Optional[int] = None
+    validity: Optional[int] = None
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
+    is_active: bool = True
+
+
+class GetCourseListSchema(Schema):
+    id: UUID
     name: str
     description: str = None
     additional_details: Optional[str] = None

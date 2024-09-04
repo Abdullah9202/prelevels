45cdab41aa50fa9509e3a3eeb998be0cfa5dd72c
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-04 19:20:52 +0500

    redundant app removed

--- server/authentication/api.py
+++
@@ -1,9 +0,0 @@
-from ninja import NinjaAPI
-# My Files
-from .router import auth_router
-
-# API Init
-api = NinjaAPI(version="1.0.0", urls_namespace="authentication")
-
-# Including the auth_router
-api.add_router("", auth_router)

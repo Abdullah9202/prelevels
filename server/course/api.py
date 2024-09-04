651a25cfbb5e2b62c9cfbebe59e9491fb098ce09
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-04 06:28:09 +0500

    api router and controller added for course

--- server/course/api.py
+++ server/course/api.py
@@ -1,7 +1,14 @@
-from ninja import NinjaAPI
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.controller import NinjaJWTDefaultController
 # My Files
 from .schemas import CourseSchema
 
 
 # API init
-api = NinjaAPI(version="1.0.0", urls_namespace="course")
+api = NinjaExtraAPI(version="1.0.0", urls_namespace="course")
+# Auth Token
+api.register_controllers(NinjaJWTDefaultController)
+
+
+# Including the router from course
+api.add_router("", "course.router.course_router")

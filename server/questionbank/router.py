bbe9df300f415767855051addd76cf3847020845
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-06 07:47:29 +0500

    new routers added + minor fixations

--- server/questionbank/router.py
+++ server/questionbank/router.py
@@ -11,8 +11,13 @@ from ninja.errors import HttpError
 from ninja_jwt.authentication import JWTAuth
 from ninja.responses import codes_4xx
 # My Files
-from .models import QuestionBank
-from .schemas import QuestionBankSchema, QuestionBankDetailSchema
+from .models import (
+    QuestionBank, Question
+)
+from .schemas import (
+    QuestionBankSchema, QuestionBankDetailSchema, QuestionSchema, QuestionDetailSchema,
+    OptionSchema, WhyCorrectOptionSchema
+)
 from .serializers import QuestionBankSerializer
 
 
@@ -22,7 +27,7 @@ question_bank_router = Router()
 # Set up logging
 logger = logging.getLogger(__name__)
 
-# Get all question banks router
+# Get all question banks
 @question_bank_router.get("", response={200: List[QuestionBankSchema], codes_4xx: dict}, auth=JWTAuth())
 def get_all_question_banks(request, *args, **kwargs):
     try:
@@ -40,7 +45,7 @@ def get_all_question_banks(request, *args, **kwargs):
         raise HttpError(500, "An unexpected error occurred. Please try again later.")
 
 
-# Get question bank details router
+# Get the details of specific Question bank
 @question_bank_router.get("/{question_bank_id}", response={200: QuestionBankDetailSchema, codes_4xx: dict}, auth=JWTAuth())
 def get_question_bank_details(request, question_bank_id: UUID, *args, **kwargs):
     try:
@@ -56,3 +61,83 @@ def get_question_bank_details(request, question_bank_id: UUID, *args, **kwargs):
     except Exception as err:
         logger.error(f"Unexpected error: {str(err)}")
         raise HttpError(500, "An unexpected error occurred. Please try again later.")
+
+
+# Get all questions in a question bank
+@question_bank_router.get("/{question_bank_id}/all-questions", response={200: QuestionSchema, codes_4xx: dict}, auth=JWTAuth())
+def get_questions_in_question_bank(request, question_bank_id, *args, **kwargs):
+    # Getting the question banks using id
+    question_bank = get_object_or_404(QuestionBank, id=question_bank_id)
+
+    # Getting the questions along with their options and why correct option from question bank
+    questions = Question.objects.filter(question_bank=question_bank).prefetch_related('options', 'options__why_correct_option')
+
+    # Creating a list of questions with option and why correct option
+    detailed_question = []
+    for question in questions:
+        options = []
+        for option in question.options.all():
+            why_correct = None
+            if option.why_correct_option:
+                why_correct = WhyCorrectOptionSchema.from_orm(option.why_correct_option).dict()
+            options.append(OptionSchema.from_orm(option).dict() | {"why_correct_option": why_correct})
+        
+        detailed_question.append(QuestionDetailSchema.from_orm(question).dict() | {"options": options})
+    
+    # Structuring the response data
+    response_data = {
+        "id": str(question_bank.id), 
+        "name": question_bank.name, 
+        "question_bank_image": question_bank.question_bank_image.url if question_bank.question_bank_image else None,
+        "description": question_bank.description,
+        "additional_details": question_bank.additional_details,
+        "price": question_bank.price,
+        "discount": question_bank.discount,
+        "validity": question_bank.validity,
+        "created_at": question_bank.created_at,
+        "updated_at": question_bank.updated_at,
+        "is_active": question_bank.is_active,
+        "questions": detailed_question,
+    }
+
+    return JsonResponse(response_data)
+
+
+# Get the detils for specific question in a question bank
+@question_bank_router.get("/{question_bank_id}/question/{question_id}", response={200: QuestionDetailSchema, 
+                                                                                codes_4xx: dict}, auth=JWTAuth())
+def get_question_in_question_bank(request, question_bank_id, question_id, *args, **kwargs):
+    # Getting the question bank using id
+    question_bank = get_object_or_404(QuestionBank, id=question_bank_id)
+    # Getting the specific question along with it's options and why correct option from question bank
+    question = get_object_or_404(Question, id=question_id, question_bank=question_bank)
+
+    # Question with option and it's why correct option
+    options = []
+    for option in question.options.all():
+        why_correct = None
+        if option.why_correct_option:
+            why_correct = WhyCorrectOptionSchema.from_orm(option.why_correct_option).dict()
+        options.append(OptionSchema.from_orm(option).dict() | {"why_correct_option": why_correct})
+
+    
+    # Structuring the response data
+    response_data = {
+        "id": str(question.id),
+        "question_bank_id": str(question_bank.id),
+        "year": question.year,
+        "category": question.category,
+        "subject": question.subject,
+        "topic": question.topic,
+        "question_number": question.question_number,
+        "question_text": question.question_text,
+        "question_image": question.question_image.url if question.question_image else None,
+        "additional_details": question.additional_details,
+        "unique_identifier": question.unique_identifier,
+        "created_at": question.created_at,
+        "updated_at": question.updated_at,
+        "is_active": question.is_active,
+        "options": options,
+    }
+    
+    return JsonResponse(response_data)

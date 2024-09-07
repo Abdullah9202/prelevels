95259c178245777f1954621b8f12eb0ac117e7eb
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-07 11:55:05 +0500

    NinjaExtra API added

--- server/cart/api.py
+++ server/cart/api.py
@@ -1,6 +1,12 @@
-from ninja import NinjaAPI
-# My Files
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.controller import NinjaJWTDefaultController
 
 
 # API Init
-api = NinjaAPI()
+api = NinjaExtraAPI()
+# Auth Token
+api.register_controllers(NinjaJWTDefaultController)
+
+
+# Including the router from cart app
+api.add_router("", "cart.router.cart_router")

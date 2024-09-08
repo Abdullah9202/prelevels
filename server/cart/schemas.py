044836c3859448b4c305fb14cb0f78627e6b44c1
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-08 17:37:44 +0500

    Schema updated

--- server/cart/schemas.py
+++ server/cart/schemas.py
@@ -1,14 +1,17 @@
 from ninja import Schema
 from uuid import UUID
 
+
 class CartItemSchema(Schema):
     product_id: UUID
     product_model: str
     quantity: int
+
 
 class CartResponseSchema(Schema):
     id: UUID
     product_id: UUID
     product_name: str
     quantity: int
+    price: int
     created_at: str

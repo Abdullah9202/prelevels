d93482177f54cff8577aab286d91a51cf0717613
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-09 10:40:17 +0500

    All Serializers for questionbank models has been added

--- server/questionbank/serializers.py
+++ server/questionbank/serializers.py
@@ -1,13 +1,46 @@
 from rest_framework import serializers
-# My Files
-from .models import QuestionBank
+from .models import QuestionBank, Question, Option, WhyCorrectOption, Report
 
 
-# Serializer for question bank model
 class QuestionBankSerializer(serializers.ModelSerializer):
     class Meta:
         model = QuestionBank
         fields = [
             'id', 'name', 'question_bank_image', 'description', 'additional_details', 'price', 'discount',
             'validity', 'created_at', 'updated_at', 'is_active',
+        ]
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = [
+            'id', 'question_bank_id', 'year', 'category', 'subject', 'topic', 'question_number',
+            'question_text', 'question_image', 'additional_details', 'unique_identifier', 'created_at',
+            'updated_at', 'is_active',
+        ]
+
+
+class OptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Option
+        fields = [
+            'id', 'question_id', 'option_text', 'is_correct', 'created_at', 'updated_at', 'is_active',
+            'why_correct_option_id',
+        ]
+
+
+class WhyCorrectOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WhyCorrectOption
+        fields = [
+            'id', 'why_correct_option_text', 'created_at', 'updated_at', 'is_active',
+        ]
+
+
+class ReportSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Report
+        fields = [
+            'id', 'question_id', 'comment', 'created_at',
         ]

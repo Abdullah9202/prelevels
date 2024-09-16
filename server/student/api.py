e4ee6cb0292d75bf0ff2fb898d0a5138c3b7d94f
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-16 08:46:04 +0500

    auth token removed

--- server/student/api.py
+++ server/student/api.py
@@ -1,16 +1,8 @@
 from ninja_extra import NinjaExtraAPI
-from ninja_jwt.controller import NinjaJWTDefaultController
-# My files
-from .schemas import (
-    StudentSchema, Error
-)
 
 
 # API init
 api = NinjaExtraAPI(version="1.0.0", urls_namespace="student")
-# Auth Token
-api.register_controllers(NinjaJWTDefaultController)
-
 
 # Including the auth_router form Student
 api.add_router("", "student.router.auth_router")

9d3ae8ea623c4688c5e0a358c0e11df78e99e940
Abdullah-Zulfikar <abdullahzulfiqar123apple@gmail.com>
2024-09-17 11:07:03 +0500

    feat:api code added

--- client/src/app/(pages)/courses/page.tsx
+++ client/src/app/(pages)/courses/page.tsx
@@ -5,8 +5,28 @@ import Sidebar from "@/components/ui/sidebar";
 import Footer from "@/components/main/footer";
 import Belowfooter from "@/components/main/belowfooter";
 import Courses from "@/assets/Courses.png"
+import { useEffect, useState } from "react";
 
 export default function CoursesPage() {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const fetchPost = async () => {
+            try{
+                const res = await fetch("your python server link",{ method: 'GET' });
+                const data = await res.json();
+                if(res.ok) {
+                    setPosts(data);
+                    console.log('Posts fetched:', data);
+                } else{
+                    console.log('No data found');
+                }
+            }catch(error){
+                console.error('Error fetching posts:', error);
+            }
+        }
+    }
+    , []);
+
     return(
         <div>
         <SeasonSale />

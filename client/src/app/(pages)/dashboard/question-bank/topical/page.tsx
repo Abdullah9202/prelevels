03078f5e49575a9948154594d08c53e7ccd8cf69
Abdullah-Zulfikar <abdullahzulfiqar123apple@gmail.com>
2024-09-26 20:11:28 +0500

    Refactor code to import SolveQuestion component in page.tsx and created the topical and solve pages

--- client/src/app/(pages)/dashboard/question-bank/topical/page.tsx
+++ client/src/app/(pages)/dashboard/question-bank/topical/page.tsx
@@ -1,13 +1,13 @@
 import Sidebar from "@/components/dashboard/sidebar"
 
-import SolveQuestion from "@/components/dashboard/questionbank/solve/page"
+import TopicalPage from "@/components/dashboard/questionbank/topical/page"
 
 
 export default function Solve() {
     return (
         <div className="min-h-screen flex flex-col md:flex-row bg-gray-100">
             <Sidebar />
-            <SolveQuestion  />
+            <TopicalPage  />
         </div>
     )
 

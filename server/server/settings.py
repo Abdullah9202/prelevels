0d66fa2b36b8f06879c1970edfb9b4edaed81eb5
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-09-28 15:13:06 +0500

    rest_framework app added

--- server/server/settings.py
+++ server/server/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # 3rd party apps
+    'rest_framework',
     'corsheaders',
     'ninja_extra',
     # Main App

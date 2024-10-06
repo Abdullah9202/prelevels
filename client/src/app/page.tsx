111e8c891a31f19e8ed018e4185b1b1614d44fd7
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-10-06 13:11:22 +0500

    Refactor: Un used code removed

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -18,58 +18,7 @@ export default function Home() {
   const { getToken } = useAuth();
   
   const { isSignedIn, user } = useUser();  // Destructure to get both isSignedIn and user
-  
-  // V1
-  // useEffect(() => {
-  //   const sendUserIdToBackend = async () => {
-  //     if (isSignedIn && user) {
-  //       const sessionInitialized = localStorage.getItem("sessionInitialized");
 
-  //       if (!sessionInitialized) {
-  //         try {
-  //           const body = JSON.stringify({ user_id: user.id });
-
-  //           const response = await fetch("http://127.0.0.1:8000/api/student/init-session/", {
-  //             method: "POST",
-  //             headers: {
-  //               "Content-Type": "application/json",
-  //             },
-  //             body: body,
-  //           });
-
-  //           if (!response.ok) {
-  //             throw new Error(`HTTP error! status: ${response.status}`);
-  //           }
-
-  //           const data = await response.json();
-  //           console.log("User ID sent successfully:", data);
-
-  //           if (data.session_active) {
-  //             // Updating the local storage to indicate that the session has been initialized
-  //             localStorage.setItem("sessionInitialized", "true");
-  //           } else {
-  //             // If session is not active, remove the sessionInitialized flag
-  //             localStorage.removeItem("sessionInitialized");
-  //           }
-  //         } catch (error) {
-  //           console.error("Error sending user ID:", error);
-  //         }
-  //       }
-  //     }
-  //   };
-
-  //   // Send the userId if signed in
-  //   sendUserIdToBackend();
-
-  //   // Cleanup function to remove session when user logs out
-  //   return () => {
-  //     if (!isSignedIn) {
-  //       localStorage.removeItem("sessionInitialized");
-  //     }
-  //   };
-  // }, [isSignedIn, user]);
-
-  // V2
   const initializeSession = useCallback(async () => {
     try {
       // Check if session has already been initialized

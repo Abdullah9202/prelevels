61cfe06c49c5987342c014b213077c2e470af1fd
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-10-15 20:50:01 -0700

    Refactor: Search bar modified

--- client/src/components/ui/searchbar.tsx
+++ client/src/components/ui/searchbar.tsx
@@ -4,8 +4,8 @@ export default function SearchBar() {
       <div className="relative w-full max-w-md">
         <input
           type="text"
-          placeholder="Search..."
-          className="w-full py-3 pl-6 pr-32 text-gray-700 border border-gray-300 rounded-2xl focus:outline-none focus:ring-2 focus:ring-red-500"
+          placeholder="Find question bank"
+          className="w-full py-4 pl-6 pr-32 text-gray-700 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-red-500"
         />
         <button className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-red-500 text-white px-5 py-2 rounded-3xl hover:bg-red-600 focus:outline-none">
           Search

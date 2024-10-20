243a0a1844ef298ffd990cb1329ee24eb46b48d2
Abdullah-Zulfikar <B22F0275AI131@fecid.paf-iast.edu.pk>
2024-10-20 03:48:38 -1000

    Refactor: Update Test page layout and add responsive banner component

--- client/src/components/questionbank/banner.tsx
+++ client/src/components/questionbank/banner.tsx
@@ -7,7 +7,7 @@ interface BannerProps {
 
 const Banner = ({ src, alt }: BannerProps) => {
   return (
-    <div className="flex justify-center items-center py-5 px-0 sm:px-6 md:px-0 lg:px-0 mx-auto w-full max-w-screen-lg lg:max-w-screen-2xl">
+    <div className="flex justify-center items-center py-5 px-0 sm:px-6 md:px-0 lg:px-0 xl:px-0 mx-auto w-full max-w-screen-lg lg:max-w-[1760px] xl:max-w-[2000px]">
       <Image
         src={src}
         alt={alt}
@@ -15,8 +15,8 @@ const Banner = ({ src, alt }: BannerProps) => {
         width={700} // The image's natural aspect ratio width
         height={400} // The image's natural aspect ratio height
         className="object-cover"
-        sizes="(max-width: 640px) 100vw, (max-width: 768px) 90vw, (max-width: 1024px) 80vw, 700px" 
-        // Adjust sizes for small, medium, and large screens for mouse devices
+        sizes="(max-width: 640px) 100vw, (max-width: 768px) 90vw, (max-width: 1024px) 80vw, (max-width: 1280px) 70vw, (max-width: 2000px) 60vw, 50vw" 
+        // Adjust sizes for small, medium, large, extra-large, and 2xl screens
       />
     </div>
   );

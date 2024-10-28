df7f35e6349770626270cc64a486c5d1c6397aac
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-10-28 08:57:00 +0500

    csrf token added

--- server/customuser/api.py
+++ server/customuser/api.py
@@ -1,7 +1,7 @@
 from ninja_extra import NinjaExtraAPI
 
 # API init
-api = NinjaExtraAPI(version="1.0.0", urls_namespace="user") # AZAK > Test CSRF in API routes
+api = NinjaExtraAPI(version="1.0.0", urls_namespace="user", csrf=True) # AZAK > Test CSRF in API routes
 
 # Including the auth_router form User
 api.add_router("", "customuser.router.auth_router")

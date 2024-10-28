6e47fa3a84b06ef5ac110a9ab8da8304786fa016
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-10-28 08:58:22 +0500

    Feat: Register serializer created

--- server/customuser/serializers.py
+++ server/customuser/serializers.py
@@ -1,29 +1,54 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 # My Files
 from .models import User, TopicalProgress, YearlyProgress
 
 class UserSerializer(serializers.ModelSerializer):
-    # Serializer for User model
     class Meta:
         model = User
         fields = [
-            'id', 'clerk_id', 'first_name', 'last_name', 'avatar_url', 
-            'email', 'username', 'phone_number', 'taking_questionBanks', 
-            'taking_courses', 'taking_bundles', 'daysStreak', 'questionSolved', 
-            'questionRemained', 'createdAt', 'updatedAt', 'last_login',
+            'id', 'first_name', 'last_name', 'email', 'username', 
+            'phone_number', 'taking_questionBanks', 'taking_courses', 'taking_bundles', 'daysStreak',
+            'questionSolved', 'questionRemained', 'createdAt', 'updatedAt', 'last_login',
         ]
 
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'username', 'phone_number', 'password', 'password2']
+        
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"message": "Password fields do not match."})
+        return attrs
+        
+    def create(self, validated_data):
+        user = User.objects.create(
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email'],
+            username=validated_data['username'],
+            phone_number=validated_data['phone_number'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+
 class TopicalProgressSerializer(serializers.ModelSerializer):
-    # Serializer for TopicalProgress model
-    user = serializers.StringRelatedField()  # To represent the user as a string
+    user = serializers.StringRelatedField()
 
     class Meta:
         model = TopicalProgress
         fields = ['id', 'user', 'progress']
 
+
 class YearlyProgressSerializer(serializers.ModelSerializer):
-    # Serializer for YearlyProgress model
-    user = serializers.StringRelatedField()  # To represent the user as a string
+    user = serializers.StringRelatedField()
 
     class Meta:
         model = YearlyProgress

ffd68e03beb39e3bbc7fb39abf8ebcc16d34ed05
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-10-29 17:25:13 +0500

    New: csrf included

--- server/questionbank/api.py
+++ server/questionbank/api.py
@@ -2,7 +2,7 @@
 from ninja_extra import NinjaExtraAPI
 
 # API init
-api = NinjaExtraAPI(version="1.0.0", urls_namespace="questionbank")
+api = NinjaExtraAPI(version="1.0.0", urls_namespace="questionbank", csrf=True)
 
 # Including the router from question bank
 api.add_router("", "questionbank.router.question_bank_router")

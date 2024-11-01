6f6299d39cb435bec967f3179c12105d7feb2b27
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-11-01 19:36:55 +0500

    Refactor: Phone number field modified

--- client/src/components/main/sign-up.tsx
+++ client/src/components/main/sign-up.tsx
@@ -100,9 +100,9 @@ export default function SignUp() {
                   Phone number
                 </label>
                 <input
-                  type="number"
-                  name="phone"
-                  id="phone"
+                  type="tel"
+                  name="phone number"
+                  id="phone_number"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   onChange={handleChange}
                   placeholder="phone number"

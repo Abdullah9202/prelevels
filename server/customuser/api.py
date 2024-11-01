b50661dccd6292b4a7812582ddd6d9c50fb11388
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-11-01 19:38:40 +0500

    Refactor: CSRF removed

--- server/customuser/api.py
+++ server/customuser/api.py
@@ -1,7 +1,7 @@
 from ninja_extra import NinjaExtraAPI
 
 # API init
-api = NinjaExtraAPI(version="1.0.0", urls_namespace="user", csrf=True)
+api = NinjaExtraAPI(version="1.0.0", urls_namespace="user")
 
 # Including the auth_router form User
 api.add_router("", "customuser.router.auth_router")

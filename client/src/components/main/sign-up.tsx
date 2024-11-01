33df3f27bc4497d8cc01eb47e3f7f41a5727090e
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-11-01 20:59:47 +0500

    Refactor: From fields corrected

--- client/src/components/main/sign-up.tsx
+++ client/src/components/main/sign-up.tsx
@@ -66,7 +66,7 @@ export default function SignUp() {
                 </label>
                 <input
                   type="name"
-                  name="first name"
+                  name="first_name"
                   id="first_name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="first name"
@@ -83,7 +83,7 @@ export default function SignUp() {
                 </label>
                 <input
                   type="name"
-                  name="last name"
+                  name="last_name"
                   id="last_name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   onChange={handleChange}

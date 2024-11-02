f9a9bcb09c40b2b80efb0244de97c7290560aae6
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-11-02 13:47:41 +0500

    Feat: Category model registered with admin

--- server/bundle/admin.py
+++ server/bundle/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 # My Files
-from .models import Bundle
+from .models import BundleCategory, Bundle
+
+# BundleCategory with Admin
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at', 'updated_at', 'is_active')
+    search_fields = ('name',)
+    list_filter = ('is_active',)
+
+# Registering the BundleCategory model
+admin.site.register(BundleCategory, CategoryAdmin)
+
 
 # Bundle with Admin
 class BundleAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'discount', 'validity', 'is_active')
-    search_fields = ('name', 'description')
-    list_filter = ('course', 'question_bank')
+    list_display = ('name', 'price', 'discount', 'validity', 'is_active',)
+    search_fields = ('name', 'description',)
+    list_filter = ('course', 'question_bank',)
 
 
 # Registering the Bundle model
--- server/course/admin.py
+++ server/course/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 # My Files
-from .models import Course
+from .models import CourseCategory, Course
+
+# Category with Admin
+class CategoryAdmin(admin.ModelAdmin):
+    # Display Fields and Filters
+    list_display = ('name', 'created_at', 'updated_at', 'is_active',)
+    search_fields = ('name',)
+    list_filter = ('is_active',)
+
+# Registering the CourseCategory model
+admin.site.register(CourseCategory, CategoryAdmin)
 
 
 # Courses with Admin
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'price', 'discount', 'validity', 'is_active')
-    search_fields = ('name', 'description')
-    list_filter = ('is_active', 'price', 'validity')
+    search_fields = ('name', 'description',)
+    list_filter = ('is_active', 'price', 'validity',)
 
 
 # Registering the Course model

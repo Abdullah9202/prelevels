86ea91b6af0002a048d78e7becf31b4db8ca7d07
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-11-02 16:23:08 +0500

    Update: Course and question bank details added instead of UUIDs

--- server/bundle/serializers.py
+++ server/bundle/serializers.py
@@ -1,10 +1,15 @@
 from rest_framework import serializers
 # My Files
 from .models import Bundle
+from course.serializers import CourseSerializer
+from questionbank.serializers import QuestionBankSerializer
 
 
 # Serializer for Bundle model
 class BundleSerializer(serializers.ModelSerializer):
+    course = CourseSerializer(many=True, read_only=True)
+    question_bank = QuestionBankSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Bundle
         fields = [

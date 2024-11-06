398c9f4b4e9bbe32c7ec8c55e1a0efce843ef5ca
Abdullah-Zulfikar <B22F0275AI131@fecid.paf-iast.edu.pk>
2024-11-06 00:04:33 -1000

    feat: refactor user data handling in CoursePage and Question components; update user name display and replace img with Image component

--- client/src/components/dashboard/questionbank/question.tsx
+++ client/src/components/dashboard/questionbank/question.tsx
@@ -1,6 +1,9 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
 import { FaGoogleDrive, FaWhatsapp } from "react-icons/fa";
+"use client"
+import { useUser } from '../../../../hooks/useUser';
+import { FaGoogleDrive, FaWhatsapp } from 'react-icons/fa';
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 
@@ -37,6 +40,41 @@ export default function Question() {
       </div>
     </div>
   );
+export default function Question(){
+    
+    const router = useRouter();
+    const user = useUser((state) => state.user)
+    return (
+        <div className="flex-1 md:p-6 p-4 lg:py-7 py-4 bg-gray-100 md:mr-0 mx-auto">
+            <h1 className="md:text-3xl text-xs w-full md:w-auto font-semibold mb-4 md:mb-0 md:pb-9">
+            Welcome Back, {user.full_name} 👋
+            </h1>
+            <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
+                <CourseCard
+                title="Complete Physiology Test"
+                status="By Prelevels"
+                question='200'
+                btnText="Solve"
+                router={router}
+                />
+                <CourseCard
+                title="Complete Cardiology Test"
+                status="By Prelevels"
+                question='200'
+                btnText="Solve"
+                router={router}
+                />
+                <CourseCard
+                title="Complete Pathology Test"
+                status="By Prelevels"
+                question='200'
+                btnText="Solve"
+                router={router}
+                />
+                
+            </div>
+        </div>
+    )
 }
 
 const CourseCard = ({
@@ -53,11 +91,11 @@ const CourseCard = ({
   router: any;
 }) => (
   <div className="p-4 bg-white shadow-md rounded-lg">
-    <Image
+    {/* <Image
       src="https://via.placeholder.com/150"
       alt="Course"
       className="w-full h-32 object-cover rounded-md"
-    />
+    width={300} height={300}/> */}
     <h3 className="mt-2 font-bold">{title}</h3>
     <p className="text-sm mt-4 text-gray-500">{status}</p>
     <p className="text-sm  text-gray-500">{question}</p>
--- client/src/components/dashboard/sidebar.tsx
+++ client/src/components/dashboard/sidebar.tsx
@@ -14,7 +14,7 @@ import { useUser } from "../../../hooks/useUser";
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const router = useRouter();
-  const user = useUser((state) => state.user);
+  const user = useUser((state) => state.user)
   const dashboardUrl = user ? `/dashboard/${user.id}` : "/dashboard";
 
   const toggleSidebar = () => {
@@ -34,50 +34,24 @@ const Sidebar = () => {
       </div>
 
       {/* Sidebar */}
-      <div
-        className={`fixed inset-y-0 left-0 w-64 bg-white shadow-md transform ${
-          isOpen ? "translate-x-0" : "-translate-x-full"
-        } transition-transform duration-300 ease-in-out md:relative md:translate-x-0 md:h-full z-50`}
-      >
+      <div className={`fixed inset-y-0 left-0 w-64 bg-white shadow-md transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out md:relative md:translate-x-0 md:h-full z-50`}>
         <div className="p-6 text-2xl font-semibold text-red-600 flex items-center justify-between">
           <span className="hidden md:block">Prelevels</span>
-          <button className="px-6" onClick={() => router.push("/")}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              className="bi bi-box-arrow-left"
-              viewBox="0 0 16 16"
-            >
-              <path
-                fillRule="evenodd"
-                d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"
-              />
-              <path
-                fillRule="evenodd"
-                d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"
-              />
+          <button className='px-6' onClick={() => router.push('/')}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-arrow-left" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"/>
+              <path fillRule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"/>
             </svg>
           </button>
         </div>
         <nav className="mt-6 px-5 space-y-4">
-          <a
-            href={dashboardUrl}
-            className="flex items-center p-2 hover:bg-gray-100"
-          >
+          <a href={dashboardUrl} className="flex items-center p-2 hover:bg-gray-100">
             <FaHome size={20} className="mr-3" /> Home
           </a>
-          <a
-            href="/dashboard/courses"
-            className="flex items-center p-2 hover:bg-gray-100"
-          >
+          <a href="/dashboard/courses" className="flex items-center p-2 hover:bg-gray-100">
             <FaBook size={20} className="mr-3" /> Courses
           </a>
-          <a
-            href="/dashboard/question-bank"
-            className="flex items-center p-2 hover:bg-gray-100"
-          >
+          <a href="/dashboard/question-bank" className="flex items-center p-2 hover:bg-gray-100">
             <FaQuestion size={20} className="mr-3" /> Question Banks
           </a>
           <a href="#" className="flex items-center p-2 hover:bg-gray-100">
@@ -90,14 +64,10 @@ const Sidebar = () => {
       </div>
 
       {/* Overlay for Mobile */}
-      {isOpen && (
-        <div
-          className="fixed inset-0 bg-black opacity-50 z-40 md:hidden"
-          onClick={toggleSidebar}
-        ></div>
-      )}
+      {isOpen && <div className="fixed inset-0 bg-black opacity-50 z-40 md:hidden" onClick={toggleSidebar}></div>}
     </div>
   );
 };
 
 export default Sidebar;
+

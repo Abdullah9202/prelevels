447d9609fec750b10c16f228dcfeae1b5074017d
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-11-17 18:07:36 +0500

    Feat: update CORS settings to allow credentials

--- server/server/settings.py
+++ server/server/settings.py
@@ -208,13 +208,14 @@ NINJA_JWT = {
 
 
 # CORS Settings
+CORS_ALLOW_CREDENTIALS = True
+
 CORS_ALLOWED_ORIGINS = [ # AZAK
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
 ]
 
-CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = (
     "DELETE",

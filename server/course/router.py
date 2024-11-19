5c4bce5b3615c72abac2b6f0bdd38d93506b6e16
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-11-19 22:25:33 +0500

    Feat: Router re-arranged

--- server/course/router.py
+++ server/course/router.py
@@ -47,6 +47,31 @@ async def get_all_courses(request, *args, **kwargs):
         raise HttpError(500, "An unexpected error occurred. Please try again later.")
 
 
+# Get User Courses router
+@course_router.get("/my-courses/", response={200: dict, codes_4xx: dict}, auth=JWTAuth())
+async def get_user_courses(request, *args, **kwargs):
+    try:
+        # Getting the authenticated user
+        user = request.user
+        
+        # Getting the courses concerned with the user
+        courses = await sync_to_async(
+            lambda: list(user.taking_courses.all())
+        )()
+        
+        # Serializing the courses
+        serialized_courses = CourseSerializer(courses, many=True).data
+        
+        # Returning the Json response
+        return JsonResponse(serialized_courses, status=200, safe=False)
+    except HttpError as err:
+        logger.error(f"HttpError: {err}")
+        raise err
+    except Exception as err:
+        logger.error(f"Unexpected error: {str(err)}")
+        raise HttpError(500, "An unexpected error occurred. Please try again later.")
+
+
 # Get course details router
 @course_router.get("/{course_id}/", response={200: GetCourseDetailSchema, codes_4xx: dict}, auth=JWTAuth())
 async def get_course(request, course_id: UUID, *args, **kwargs):
@@ -59,29 +84,6 @@ async def get_course(request, course_id: UUID, *args, **kwargs):
         
         # Returning the Json data
         return JsonResponse(serialized_course, status=200)
-    except HttpError as err:
-        logger.error(f"HttpError: {err}")
-        raise err
-    except Exception as err:
-        logger.error(f"Unexpected error: {str(err)}")
-        raise HttpError(500, "An unexpected error occurred. Please try again later.")
-
-
-# Get User Courses router
-@course_router.get("/{username}/my-courses/", response={200: List[GetCourseDetailSchema], codes_4xx: dict}, auth=JWTAuth())
-async def get_user_courses(request, username: str, *args, **kwargs):
-    try:
-        # Getting the user using username
-        user = await sync_to_async(get_object_or_404)(User, username=username)
-        
-        # Getting the courses
-        courses = await sync_to_async(lambda: list(user.taking_courses.all()))()
-        
-        # Serializing the courses
-        serialized_courses = CourseSerializer(courses, many=True).data
-        
-        # Returning the Json response
-        return JsonResponse(serialized_courses, status=200, safe=False)
     except HttpError as err:
         logger.error(f"HttpError: {err}")
         raise err

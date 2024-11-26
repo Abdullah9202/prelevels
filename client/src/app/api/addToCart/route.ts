ad4ab53c32bfc90aaced98204a8fd3d9815a0928
Abdullah-Zulfikar <B22F0275AI131@fecid.paf-iast.edu.pk>
2024-11-26 05:20:57 -1000

    Fix: Correct product_model property in addToCart API request body

--- client/src/app/api/addToCart/route.ts
+++ client/src/app/api/addToCart/route.ts
@@ -9,7 +9,7 @@ export async function POST(req: NextRequest) {
         const res = await fetch("http://127.0.0.1:8000/api/cart/add/", { // AZAK
             method: "POST",
             headers : {"Content-Type": "application/json", Authorization: `Bearer ${accessToken}` },
-            body: JSON.stringify({ product_id: req_data?.product_id, product_model: req_data?.product_id, quantity:req_data?.quantity })
+            body: JSON.stringify({ product_id: req_data?.product_id, product_model: req_data?.product_model, quantity:req_data?.quantity })
         })
         
         if (res.ok){

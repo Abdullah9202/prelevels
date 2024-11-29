77b1e0569ed88cd397ca326863464ef1ffb3f066
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-11-29 07:51:18 +0500

    Refactor: Remove unnecessary blank line in admin.py

--- server/customuser/admin.py
+++ server/customuser/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User, TopicalProgress, YearlyProgress
 
+
 class UserAdmin(UserAdmin):
     model = User
     list_display = ('username', 'phone_number', 'email', 'createdAt', 'updatedAt', 'last_login')

a38eca1232c36936496c60a30c598bad4159f411
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-12-03 14:16:09 +0500

    Feat: Update logout API URL to use localhost

--- client/src/app/api/logout/route.ts
+++ client/src/app/api/logout/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 
 export async function POST(req: NextRequest) {
-  const res = await fetch('http://127.0.0.1:8000/customuser/logout/', { // AZAK
+  const res = await fetch('http://localhost:8000/api/customuser/logout/', { // AZAK
     method: "POST",
     headers: { "Content-Type": "application/json" },
     credentials: "include",

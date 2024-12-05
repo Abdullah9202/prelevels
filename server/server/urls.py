cf8edf1d0da2dfab418077bdf8d03a04f3064a8a
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-12-05 07:39:07 +0500

    Feat: Callsign added

--- server/server/urls.py
+++ server/server/urls.py
@@ -42,6 +42,6 @@ urlpatterns = [
     path('api/auth/', auth_api.urls),
 ]
 
-if settings.DEBUG:
+if settings.DEBUG: # AZAK
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

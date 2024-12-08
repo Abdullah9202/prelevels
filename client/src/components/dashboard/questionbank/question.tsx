0b395464e30ee8dadd61a13ee76aa3a900dbaf91
Abdullah-Zulfikar <B22F0275AI131@fecid.paf-iast.edu.pk>
2024-12-08 18:07:14 +0500

    Feat: Implement dynamic question bank retrieval and update CourseCard rendering

--- client/src/components/dashboard/questionbank/question.tsx
+++ client/src/components/dashboard/questionbank/question.tsx
@@ -3,9 +3,10 @@
 
 
 import { useUser } from "../../../../hooks/useUser";
-
+import { useEffect,useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
+import useHandleLogout from "@/lib/logout";
 
 
 const CourseCard = ({
@@ -13,20 +14,24 @@ const CourseCard = ({
   status,
   btnText,
   question,
+  imageUrl,
   router,
 }: {
   title: string;
   status: string;
   btnText: string;
   question: string;
+  imageUrl:string;
   router: any;
 }) => (
   <div className="p-4 bg-white shadow-md rounded-lg">
-    {/* <Image
-      src="https://via.placeholder.com/150"
+      <Image
+      src={imageUrl}
       alt="Course"
       className="w-full h-32 object-cover rounded-md"
-    width={300} height={300}/> */}
+      width={300}
+      height={300}
+    />
     <h3 className="mt-2 font-bold">{title}</h3>
     <p className="text-sm mt-4 text-gray-500">{status}</p>
     <p className="text-sm  text-gray-500">{question}</p>
@@ -44,9 +49,33 @@ const CourseCard = ({
 export default function Question() {
   
   const user = useUser((state) => state.user);
+  const [coursesList, setCoursesList] = useState<any[]>([]);
+  const [status, setStatus] = useState<number>(0);
+  const handleLogout = useHandleLogout();
+
+  useEffect(() => {
+    const getDashboardCourses = async () => {
+      const res = await fetch("/api/getDashboardQuestion_bank", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const courses = await res.json();
+      if (res.ok) {
+        setCoursesList(courses?.data);
+        setStatus(courses?.status);
+      }
+    };
+    getDashboardCourses();
+  }, []);
+
+  useEffect(() => {
+    if (status === 401) {
+      handleLogout();
+    }
+  }, [status, handleLogout]);
   
   
-  
+  console.log("these are the question banks", coursesList)
   const router = useRouter();
   return (
     <div className="flex-1 md:p-6 p-4 lg:py-7 py-4 bg-gray-100 md:mr-0 mx-auto">
@@ -54,28 +83,18 @@ export default function Question() {
         Welcome Back, {user?.firstName} 👋
       </h1>
       <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
-        <CourseCard
-          title="Complete Physiology Test"
-          status="By Prelevels"
-          question="200"
-          btnText="Solve"
-          router={router}
-        />
-        <CourseCard
-          title="Complete Cardiology Test"
-          status="By Prelevels"
-          question="200"
-          btnText="Solve"
-          router={router}
-        />
-        <CourseCard
-          title="Complete Pathology Test"
-          status="By Prelevels"
-          question="200"
-          btnText="Solve"
-          router={router}
-        />
-      </div>
+          {coursesList.map((course) => (
+            <CourseCard
+              key={course?.id}
+              title={course?.name}
+              status="by Prelevels"
+              question={course?.question}
+              btnText="Solve"
+              imageUrl={`${process.env.NEXT_PUBLIC_API_URL}${course?.image}`}
+              router={router}
+            />
+          ))}
+        </div>
     </div>
   );
 }

26ef0dbd32f2cc9ef902d0c2c7bca795896a429a
Abdullah9202 <abdullahkhurram9202@gmail.com>
2024-12-10 14:12:34 +0500

    Refactor: Update last name input field to use lowercase name attribute and capitalize placeholder text for consistency

--- client/src/components/main/sign-up.tsx
+++ client/src/components/main/sign-up.tsx
@@ -91,11 +91,11 @@ export default function SignUp() {
                 </label>
                 <input
                   type="name"
-                  name="Last_name"
+                  name="last_name"
                   id="last_name"
                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   onChange={handleChange}
-                  placeholder="last name"
+                  placeholder="Last name"
                   required
                 />
               </div>
